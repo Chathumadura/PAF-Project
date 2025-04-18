fe358762c0a9f08dad39f32b04c3ae0fea182f0c
Chathumadura <kavishka.chathumadura250@gmail.com>
2025-04-18 14:21:29 +0530

    first done

---
+++ src/test/java/com/pafproject/pafproject/PafprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pafproject.pafproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
